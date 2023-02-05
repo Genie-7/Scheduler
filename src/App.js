3fa790c418f241426796f1b59c802dc27944b494
Chengyue Wei <weiyuezhong2012@gmail.com>
2023-02-05 12:19:46 -0500

    Initialize with react library

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { useState } from 'react';
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 import './App.css';
 
 function App() {
+  const [date, setDate] = useState(new Date());
+
   return (
-    <div>
-      <h1>React App</h1>
-      <p>My first React App</p>  
+    <div className='app'>
+      <h1 className='text-center'>React Calendar</h1>
+      <div className='calendar-container'>
+        <Calendar onChange={setDate} value={date} />
+      </div>
+      <p className='text-center'>
+        <span className='bold'>Selected Date:</span>{' '}
+        {date.toDateString()}
+      </p>
     </div>
   );
 }
